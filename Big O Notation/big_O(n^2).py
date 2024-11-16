e83d5922db6ca746d856e616d43815640ad14981
Mani-420 <tahirabdulrehman46@gmail.com>
2024-11-16 11:42:39 +0500

    big_O(n^2)

---
+++ Big O Notation/big_O(n^2).py
@@ -0,0 +1,21 @@
+# O(n^2) Quadratic Time ----------------------------------------
+# Insertion sort
+# Selection sort
+# Bubble sort
+# Too Slow. Even with less data takes too much time.
+
+
+# Traverse a square grid
+nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for i in range(len(nums)):
+    for j in range(len(nums[i])): 
+        print(nums[i][j])
+
+
+# Get every pair of elements in array
+nums = [1, 2, 3]
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        print(nums[i], nums[j])
+
+# Insertion sort (insert in middle n times -> n^2)
