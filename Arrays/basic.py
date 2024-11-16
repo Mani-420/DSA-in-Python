9e01ded0a94222115c919db55f1086d0d2752f21
Mani-420 <tahirabdulrehman46@gmail.com>
2024-11-16 16:01:40 +0500

    Arrays

---
+++ Arrays/basic.py
@@ -0,0 +1,11 @@
+# Initialization----------------------------------------------
+import array as arr
+
+arr1 = arr.array("i", [1,2,3,4,5])
+print(arr1)
+
+
+# Accessing Elements ----------------------------------------------
+
+arr2 = arr.array("i", [1,2,3,4,5])
+print(arr2[1])
