33a4496d50682a0288b8b88e00efa8ed25ee009e
Mani-420 <tahirabdulrehman46@gmail.com>
2024-11-19 16:04:47 +0500

    Rotating Array Right

--- Arrays_in_Python/rotate_array.py
+++ Arrays_in_Python/rotate_array.py
@@ -11,3 +11,17 @@ for i in original:
     temp_arr.append(original[i])
 
 print(temp_arr)
+
+
+# Leetcode Problem 189:
+# class Solution(object):
+#     def rotate(self, nums, k):
+#         n = len(nums)
+#         k %= n  # Handle cases where k > n
+#         # Reverse the entire array
+#         nums.reverse()
+#         # Reverse the first k elements
+#         nums[:k] = reversed(nums[:k])
+#         # Reverse the remaining n-k elements
+#         nums[k:] = reversed(nums[k:])
+        
